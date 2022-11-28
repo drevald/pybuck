b4a8b04eb0d4fcc01096d557255b95483c26f723
drevald <ddreval@gmail.com>
2022-11-29 01:31:17 +0300

    requirements added

--- create.sh
+++ create.sh
@@ -4,7 +4,6 @@ echo 'START OF SCRIPT!!!'
 
 aws configure set aws_access_key_id xxx
 aws configure set aws_secret_access_key xxx
-aws —endpoint-url=http://localhost:4566 s3api create-bucket —bucket mybuck —region us-east-1
-aws —endpoint-url=http://localhost:4566 s3api create-bucket —bucket mybuck2 —region us-east-1
+aws --endpoint-url=http://localhost:4566  s3api create-bucket --bucket my-bucket --region us-east-1
 
 echo 'END OF SCRIPT!!!'
