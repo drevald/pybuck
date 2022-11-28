37d337e0319708d30116b5915f595f70baf2fa75
drevald <ddreval@gmail.com>
2022-11-29 02:13:07 +0300

    test is running fine

---
+++ README.md
@@ -0,0 +1,2 @@
+pilot creates emulation of source with s3 protocol support - using localstack
+when s3 source is created run client.py to put and get something from the bucket using s3 protocol
--- client.py
+++ client.py
@@ -1,3 +1,19 @@
 import boto3
 
-client = boto3.client('s3')
+client = boto3.client('s3',
+        aws_access_key_id='xxx',
+        aws_secret_access_key= 'xxx',
+        endpoint_url='http://192.168.0.198:4566', # my laptop address where docker engine is running
+        region_name='us-east-1')
+
+response = client.list_buckets()
+
+assert (response['Buckets'][0]['Name']) == 'my-bucket'  # check that bucket exist
+
+client.put_object(Bucket='my-bucket', Key='something', Body='three apples two lemons')  # put something to it
+
+bucket_response = client.get_object(Bucket='my-bucket', Key='something') # get something from it
+
+body = bucket_response['Body']
+
+assert (body.read()).decode('utf-8') == 'three apples two lemons' # check stored content
