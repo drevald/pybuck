f5a191bcfef8c7782109dabbc33ba39b8b9ad982
unknown <ddreval@gmail.com>
2022-11-28 22:05:58 +0000

    init

---
+++ create.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+echo 'START OF SCRIPT!!!'
+
+aws configure set aws_access_key_id xxx
+aws configure set aws_secret_access_key xxx
+aws —endpoint-url=http://localhost:4566 s3api create-bucket —bucket mybuck —region us-east-1
+aws —endpoint-url=http://localhost:4566 s3api create-bucket —bucket mybuck2 —region us-east-1
+
+echo 'END OF SCRIPT!!!'
